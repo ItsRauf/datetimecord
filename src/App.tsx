398d609d8559976843a8eb25effd18f29a7eef20
ItsRauf <raufislam2003@gmail.com>
2021-06-29 20:30:36 -0400

    github

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import DateTimePicker from "@material-ui/lab/DateTimePicker";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import dayjs, { Dayjs } from "dayjs";
 
 function App() {
+  const [date, setDate] = useState<Dayjs | null>(dayjs());
+  const [format, setFormat] = useState("f");
+  const [output, setOutput] = useState("");
+
+  useEffect(() => {
+    if (date) {
+      setOutput(`<t:${date.unix()}:${format}>`);
+    } else {
+      setOutput("");
+    }
+  }, [date, format]);
+
+  const changeFormat = (event: React.ChangeEvent<{ value: string }>) => {
+    setFormat(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ height: "100vh" }} fixed>
+      <Grid
+        container
+        sx={{ height: "100%", alignItems: "center", justifyContent: "center" }}
+      >
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Grid
+                container
+                direction="column"
+                spacing={4}
+                sx={{
+                  height: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  paddingTop: "15px",
+                }}
+              >
+                <Grid item>
+                  <Typography variant="h6" gutterBottom component="h6">
+                    DateTimeCord
+                  </Typography>
+                  <Typography variant="subtitle2" gutterBottom component="p">
+                    Helper Tool for Date and Time formatting in Discord
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Grid
+                    container
+                    spacing={4}
+                    sx={{
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Grid item xs={7}>
+                      <DateTimePicker
+                        renderInput={(props) => (
+                          <TextField {...props} helperText="" />
+                        )}
+                        label="Date & Time"
+                        value={date}
+                        onChange={(newValue) => {
+                          setDate(newValue);
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={5}>
+                      <FormControl fullWidth>
+                        <InputLabel id="datetimecord-format">Format</InputLabel>
+                        <Select
+                          labelId="datetimecord-format"
+                          id="datetimecord-format"
+                          value={format}
+                          label="Format"
+                          onChange={changeFormat}
+                        >
+                          <MenuItem value="t">Short Time</MenuItem>
+                          <MenuItem value="T">Long Time</MenuItem>
+                          <MenuItem value="d">Short Date</MenuItem>
+                          <MenuItem value="D">Long Date</MenuItem>
+                          <MenuItem value="f">Short Date/Time</MenuItem>
+                          <MenuItem value="F">Long Date/Time</MenuItem>
+                          <MenuItem value="R">Relative Time</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Paper elevation={3} sx={{ padding: "16px" }}>
+                    <Typography variant="subtitle1" component="p">
+                      {output}
+                    </Typography>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AdapterDayJS from "@material-ui/lab/AdapterDayjs";
+import LocalizationProvider from "@material-ui/lab/LocalizationProvider";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <LocalizationProvider dateAdapter={AdapterDayJS}>
+      <ThemeProvider
+        theme={createTheme({
+          palette: {
+            mode: "dark",
+          },
+        })}
+      >
+        <App />
+      </ThemeProvider>
+    </LocalizationProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
